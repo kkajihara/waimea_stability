84c430efacdc531e4dc4404696f6a5ee71972c8c
Kacie <kaciekaj@hawaii.edu>
2023-04-04 22:27:00 +0000

    upload network scripts

---
+++ networks/prev50_finals/r_scripts/crossdom_mb_defaults.R
@@ -0,0 +1,26 @@
+library(SpiecEasi)
+library(batchtools)
+
+setwd("~/cmaiki_lts/kaciekaj/waimea/networks/prev50_finals/r_scripts/")
+
+reg = makeRegistry(file.dir = 'batch_registry3', seed = 1)
+
+f_abun <- readRDS("~/cmaiki_lts/kaciekaj/waimea/intermediates/fully_filtered_fungal_otu_table_matched_up.rds")
+b_abun <- readRDS("~/cmaiki_lts/kaciekaj/waimea/intermediates/fully_filtered_bact_otu_table_matched_up.rds")
+
+#not real data, changing so can be same "sample names"
+names(f_abun) = 1:ncol(f_abun)
+names(b_abun) = 1:ncol(b_abun)
+
+
+f_abun <- t(f_abun)
+b_abun <- t(b_abun)
+
+#pargs <- list(seed=10010, thresh = 0.02, rep.num=1)
+#crossdom_net <- multi.spiec.easi(list(f_abun, b_abun), method='glasso', lambda.min.ratio=1e-1, pulsar.select=FALSE, scr=TRUE)
+
+bargs <- list(conffile=".batchtools.conf.R")
+crossdom_net <- multi.spiec.easi(list(f_abun, b_abun), method='mb', scr=TRUE,
+                             pulsar.select='batch', pulsar.params=bargs)
+
+saveRDS(crossdom_net, "~/cmaiki_lts/kaciekaj/waimea/networks/prev50_finals/outputs/crossdom_mb_nlambda20.rds")
--- networks/r_scripts/.batchtools.conf.R
+++ networks/r_scripts/.batchtools.conf.R
@@ -1,2 +1,2 @@
-cluster.functions = makeClusterFunctionsSlurm(template="slurm.tmpl")
-default.resources = list(walltime = 3600, memory = 2000, ntasks = 2, ncpus = 3, partition = "kill-exclusive")
+reg$cluster.functions = makeClusterFunctionsSlurm(template="slurm.tmpl")
+reg$default.resources = list(walltime = 3600, memory = 4000, ncpus = 1)
