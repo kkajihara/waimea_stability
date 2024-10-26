a5642d914cb5700ef9803391cffb43a3032fe794
kkajihara <kaciekaj@hawaii.edu>
2024-10-26 21:54:41 +0000

    remove host nonhost analyses

--- host_nonhost_networks/slurm/r_scripts/picante_by_sample.R
+++
@@ -1,8 +0,0 @@
-library(picante)
-
-input <- readRDS("~/hynson_koastore/kaciekaj/waimea_current/intermediates/hostnon/hostnon_inputs_for_picante_by_sample.rds")
-
-ses.mpd.result <- ses.mpd(input[[1]], input[[2]], null.model = "taxa.labels")
-
-saveRDS(ses.mpd.result, "~/hynson_koastore/kaciekaj/waimea_current/intermediates/hostnon/picante_by_sample_hostnon_results.rds")
-
